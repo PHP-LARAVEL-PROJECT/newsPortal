4a90e7f8ef3105e182a4b0bfafe32e199c800189
Anish <anishgharti10@gmail.com>
2023-08-12 00:09:14 +0545

    feat:learning routes , groups in routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('anish', function () {
+    return 'this is  a anish page';
+
+});
+
+
+Route::get('person/{id}', function (){
+
+return "anish";
+});
+
+Route:: get('animals', function(){
+    return 'dogs and cats';
+})->name('animals');
+
+
+Route:: group(['as'=> 'animals.'], function(){
+    Route:: get('meat', function(){
+        return 'chicken tanduri';
+    })->name('meat');
+    Route:: get('sexy girl', function(){
+        return 'hot girls';
+    })->name('sexy');
+});
